254ba84b07773a83fb98e9884c33a8f2a0b1c83c
Charbel t <charbel2000.52@gmail.com>
2020-11-15 10:33:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,4 +5,5 @@ document.querySelector('main').remove();
 
 const newHeader = document.createElement('h1');
 newHeader.id='victory';
+newHeader.innerHTML = "YOUR-NAME is the champion"
 document.body.appendChild(newHeader);
